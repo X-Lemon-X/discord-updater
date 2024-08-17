c7287b82562103a3b32f11515df26512b3f6b307
Patryk Dudzinski (LemonX) <dudzinski.patryk.02@gmail.com>
2024-08-17 16:54:02 +0200

    added vecnor installe d nad withced to sue sudo instead of root

--- updater-discord.sh
+++ updater-discord.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
-  echo "This script must be run as root. Use sudo"
-  echo "Exiting..."
-  exit 1
-fi
+# if [[ $EUID -ne 0 ]]; then
+#   echo "This script must be run as root. Use sudo"
+#   echo "Exiting..."
+#   exit 1
+# fi
 directory=$(dirname "$(readlink -f "$0")")
 cd $directory
-python3 discord-updater.py
+sudo python3 discord-updater.py
+
+set -e
+outfile=$(mktemp)
+trap 'rm -f "$outfile"' EXIT
+echo "Downloading vencord Installer..."
+set -- "XDG_CONFIG_HOME=$XDG_CONFIG_HOME"
+curl -sS https://github.com/Vendicated/VencordInstaller/releases/latest/download/VencordInstallerCli-Linux \
+  --output "$outfile" \
+  --location 
+chmod +x "$outfile"
+sudo env "$@" "$outfile" -install -location /usr/share/discord
